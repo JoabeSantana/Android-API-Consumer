e8105b50a8e10d7565c3d2a14bce9e32fecfd666
Joabe Santana <joabesantana.c@gmail.com>
2024-05-08 21:57:55 -0300

    Initial Commit

---
+++ app/src/main/java/com/joabesantana/androidapiconsumer/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.joabesantana.androidapiconsumer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
