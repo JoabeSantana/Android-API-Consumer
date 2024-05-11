5947625568c2ba081e77b1ef27ccc23244791ead
Joabe Santana <joabesantana.c@gmail.com>
2024-05-11 13:06:27 -0300

    Changing to GridView

--- app/src/main/java/com/joabesantana/androidapiconsumer/ListPhotoActivity.kt
+++ app/src/main/java/com/joabesantana/androidapiconsumer/ListPhotoActivity.kt
@@ -1,39 +1,39 @@
 package com.joabesantana.androidapiconsumer
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import androidx.recyclerview.widget.RecyclerView.LayoutManager
+import android.widget.GridView
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
 class ListPhotoActivity : AppCompatActivity() {
 
-    private lateinit var recycleView: RecyclerView
+    private lateinit var gridView: GridView
     private lateinit var adapter: PhotoAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list_photo)
 
-        recycleView = findViewById(R.id.recycleView)
+        gridView = findViewById(R.id.idGridViewPhotos)
 
-        adapter = PhotoAdapter(emptyList())
-        recycleView.layoutManager = LinearLayoutManager(this)
+        //adapter = PhotoAdapter(emptyList())
+        //gridView.layoutManager = LinearLayoutManager(this)
         //recycleView.adapter = adapter
 
         //val photoService = RetrofitClient.createPhotoService()
         val photoService = RetrofitClient.createService(IPhotoService::class.java)
         val call = photoService.fetchPhotos()
+        val context : Context  = this
         call.enqueue(object : Callback<List<Photo>>{
             override fun onResponse(call: Call<List<Photo>>, response: Response<List<Photo>>) {
                 if (response.isSuccessful) {
                     val photos = response.body()
                     if (photos != null) {
-                        adapter = PhotoAdapter(photos)
-                        recycleView.adapter = adapter
+                        adapter = PhotoAdapter(context, photos)
+                        gridView.adapter = adapter
                         adapter.notifyDataSetChanged()
                     }
                 } else {
--- app/src/main/java/com/joabesantana/androidapiconsumer/PhotoAdapter.kt
+++ app/src/main/java/com/joabesantana/androidapiconsumer/PhotoAdapter.kt
@@ -1,36 +1,63 @@
 package com.joabesantana.androidapiconsumer
 
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import android.widget.ImageView
 import android.widget.TextView
-import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 
-class PhotoAdapter(private val photos: List<Photo>): RecyclerView.Adapter<PhotoAdapter.PhotoViewHolder>() {
 
-    //Conecta os componestes ao adpater
-    inner class PhotoViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
-        val imageView: ImageView = itemView.findViewById(R.id.imageView)
-        val titleTextView: TextView = itemView.findViewById(R.id.titleTextView)
-    }
+class PhotoAdapter(context: Context, photos: List<Photo>) :
+    ArrayAdapter<Photo?>(context, 0, photos) {
 
-    //Infla meu XML
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PhotoViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_photo, parent, false)
-        return PhotoViewHolder(view)
-    }
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
 
-    override fun getItemCount(): Int {
-        return photos.size
-    }
+        var itemListView = convertView
+        if (itemListView == null) {
+            itemListView = LayoutInflater.from(context).inflate(R.layout.item_photo, parent, false)
+        }
 
-    // Conecta os dados com os nossos componente
-    override fun onBindViewHolder(holder: PhotoViewHolder, position: Int) {
-        val photo = photos[position]
-        holder.titleTextView.text = photo.title
-        Glide.with(holder.itemView.context).load(photo.thumbnailUrl).centerCrop()
-            .into(holder.imageView)
+        val photo: Photo? = getItem(position)
+
+        val titleTextView: TextView = itemListView!!.findViewById(R.id.titleTextView)
+        val imageView: ImageView = itemListView.findViewById(R.id.imageView)
+
+        titleTextView.text = photo!!.title
+        Glide.with(context).load(photo!!.thumbnailUrl).centerCrop().into(imageView)
+
+        itemListView.setOnClickListener {
+            println("${photo.title}")
+        }
+        return itemListView
     }
 }
+
+//class PhotoAdapter(private val photos: List<Photo>): RecyclerView.Adapter<PhotoAdapter.PhotoViewHolder>() {
+//
+//    //Conecta os componestes ao adpater
+//    inner class PhotoViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+//        val imageView: ImageView = itemView.findViewById(R.id.imageView)
+//        val titleTextView: TextView = itemView.findViewById(R.id.titleTextView)
+//    }
+//
+//    //Infla meu XML
+//    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PhotoViewHolder {
+//        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_photo, parent, false)
+//        return PhotoViewHolder(view)
+//    }
+//
+//    override fun getItemCount(): Int {
+//        return photos.size
+//    }
+//
+//    // Conecta os dados com os nossos componente
+//    override fun onBindViewHolder(holder: PhotoViewHolder, position: Int) {
+//        val photo = photos[position]
+//        holder.titleTextView.text = photo.title
+//        Glide.with(holder.itemView.context).load(photo.thumbnailUrl).centerCrop()
+//            .into(holder.imageView)
+//    }
+//}
